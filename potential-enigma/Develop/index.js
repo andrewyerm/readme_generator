70fef73120c08eb3e7a3718623159b7cee01c98a
andrewyerm <yermakovichandrew@gmail.com>
2024-08-28 17:13:54 -0400

    push code

--- potential-enigma/Develop/index.js
+++ potential-enigma/Develop/index.js
@@ -1,13 +1,77 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// These questions will guide the user's input for generating the README file
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What are the installation instructions?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide usage information:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide contribution guidelines:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Writes the generated markdown content to a README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.error(`Error writing file: ${err}`) : console.log('README.md generated successfully!')
+  );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+// Initializes the app by prompting the user with questions
+function init() {
+  inquirer.prompt(questions).then((responses) => {
+    // Generate the markdown content using user responses
+    const markdownContent = generateMarkdown(responses);
+    // Write the generated content to a README file
+    writeToFile('README.md', markdownContent);
+  });
+}
 
 // Function call to initialize app
 init();
--- potential-enigma/Develop/utils/generateMarkdown.js
+++ potential-enigma/Develop/utils/generateMarkdown.js
@@ -1,19 +1,76 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  }
+  // Replace spaces with underscores for badge URL compatibility
+  const formattedLicense = license.replace(' ', '_');
+  return `![License Badge](https://img.shields.io/badge/License-${formattedLicense}-blue.svg)`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  }
+  // Adds the license link to the table of contents
+  return `- [License](#license)`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  }
+  // Describes the license being used for the project
+  return `## License
+
+This project is licensed under the ${license} license.`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+${renderLicenseLink(data.license)}
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+Have questions or need help? You can reach out to me at ${data.email}. Feel free to check out more of my work on GitHub: [${data.github}](https://github.com/${data.github}).
 `;
 }
 
